bf8aaad1e769d082b7d571d72a9185a9d2abd1c4
talarisulochana <talarisulochana123@gmail.com>
2024-11-05 20:13:24 +0530

    Add files via upload

---
+++ doc.js
@@ -0,0 +1,15 @@
+function a() {
+    var k = document.getElementById("p");
+    k.style.fontSize = "60px";
+    k.style.backgroundColor = "red";
+    k.style.borderRadius = "50%";
+}
+function b() {
+    var c = document.getElementById("p1");
+    c.innerHTML = `
+    <img src="https://tse2.mm.bing.net/th?id=OIP.7cRYFyLoDEDh4sRtM73vvwHaDg&pid=Api&P=0&h=180" style="border-radius: 30px; margin-right: 10px;">
+     <img src="https://tse3.mm.bing.net/th?id=OIP.FEqv7YYMNjXtrVYqo7HHzAHaE7&pid=Api&P=0&h=180" style="border-radius: 100px; margin-right: 10px;">
+<img src="https://tse2.mm.bing.net/th?id=OIP.7cRYFyLoDEDh4sRtM73vvwHaDg&pid=Api&P=0&h=180" style="border-radius: 50%; margin-right: 10px;">
+    `;
+}
+
